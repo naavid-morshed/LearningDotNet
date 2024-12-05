e400c70aeddf3b23707c4c1eb7db5a1256e84cbb
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-12-05 16:47:23 +0600

    completed crud without db

---
+++ Dtos/CreateGameDto.cs
@@ -0,0 +1,8 @@
+namespace LearningDotNet.Dtos;
+
+public record class CreateGameDto(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
---
+++ Dtos/UpdateGameDto.cs
@@ -0,0 +1,8 @@
+namespace LearningDotNet.Dtos;
+
+public record class UpdateGameDto(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
