3ddca2a1852b74a03280c8ea5d9ba2b185cc797a
naavid-morshed <naavid.m@gmail.com>
2024-12-09 01:59:09 +0600

    restructured format

--- obj/Debug/net8.0/LearningDotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/LearningDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aced961535e00bf39aa8786b59f9d2baef7f4057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8b0c99503d9933307fedfb9857d74984eca848")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
