abb3b4266a07cf80ea5259222aea9df0414a1794
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-12-09 10:05:48 +0600

    restructured

--- obj/Debug/net8.0/LearningDotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/LearningDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf475ec0cedfbc10ffb963f2908a83102a0fb30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e400c70aeddf3b23707c4c1eb7db5a1256e84cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
